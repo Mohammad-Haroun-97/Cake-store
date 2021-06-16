4cb1bbe7cac6f44ec834bb08598c292d99aa07c1
Mohammadharoun <>
2021-06-16 12:37:27 +0000

    New project

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var A = prompt("What do you want buy from these Cakes; Classic cheesecake, No bake cheesecake, strawbery cheesecake )?")
+
+if(A=="Classic cheesecake"){document.write("<p> Good choice </p>")}
+
+else {document.write("<p> Bad choice </p>")}
+
+var B = prompt("This Shop require your phone number to access, Do you agree with that? )?")
+
+if(B=="yes"){alert("welcome to our shop")}
+
+
+
+
