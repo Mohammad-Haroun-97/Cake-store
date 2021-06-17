df2be559c931894a0e464503cb4b4720a64fa654
Mohammadharoun <>
2021-06-17 13:24:20 +0000

    Css

---
+++ app.js
@@ -0,0 +1,34 @@
+
+var A = prompt("What do you want buy from these Cakes; Classic cheesecake, No bake cheesecake, strawbery cheesecake )?")
+
+if(A=="Classic cheesecake"){document.write("<p> Good choice </p>")}
+
+else {document.write("<p> Bad choice </p>")}
+
+
+
+
+var B = prompt("This Shop require your phone number to access, Do you agree with that? )?")
+
+if(B=="yes"){alert("welcome to our shop")}
+
+alert("for the following question guess what is the right number from 49, 55 or 70")
+var x = prompt("guess how many kinds that we have in our shop ?");
+while (x !=49 ) {
+  x =prompt( "it is wrong answer please choose from 49, 55 ,70")}
+
+
+var numbOforder = prompt("How many pieces do you want to order ?")
+
+for (i = 0; i <= numbOforder; i++) {document.write(
+  // // "<div>" + 
+'<div> <img src="https://rakskitchen.net/wp-content/uploads/2021/04/No-bake-biscoff-cheese-1.jpg" alt="No bake cheesecake"></div>'
+  // //     "<img src='https://rakskitchen.net/wp-content/uploads/2021/04/No-bake-biscoff-cheese-1.jpg" alt="No bake cheesecake' width="500" height="250">" +
+  // //     "</div>" 
+  )
+}
+
+  
+
+
+
